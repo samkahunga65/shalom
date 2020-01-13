9b2b08562ec5eefa9df8e47a00bc82488d44f692
samkahunga65 <samkahunga65@gmail.com>
2020-01-13 18:19:02 +0300

    whoops

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('shalom_sassy.html')
 
 
 if __name__ == '__main__':
 	app.run(debug=1)
+
+
+
